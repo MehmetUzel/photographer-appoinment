3d59b6c444200804da3bc4ba5e40ccff32aaa8ad
Mehmet Uzel <98mehmetuzel@gmail.com>
2022-01-14 16:14:50 +0300

    Appointment and Offdays models creation

--- photographer/appoinment/admin.py
+++ photographer/appoinment/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Appoinment,OffDays
 
 # Register your models here.
+admin.site.register(Appoinment)
+admin.site.register(OffDays)
--- photographer/appoinment/models.py
+++ photographer/appoinment/models.py
@@ -1,3 +1,34 @@
 from django.db import models
-
+from user.models import User
 # Create your models here.
+
+class Appoinment(models.Model):
+    MORNING = 'MO'
+    NOON = 'NO'
+    EVENING = 'EV'
+    TIMES = [
+        (MORNING, 'Morning'),
+        (NOON, 'Noon'),
+        (EVENING, 'Evening'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.CharField(max_length=30, choices=TIMES)
+    creation_date = models.DateTimeField(auto_now=True)
+
+
+class OffDays(models.Model):
+    MORNING = 'MO'
+    NOON = 'NO'
+    EVENING = 'EV'
+    ALLDAY = 'ALL'
+    TIMES = [
+        (MORNING, 'Morning'),
+        (NOON, 'Noon'),
+        (EVENING, 'Evening'),
+        (ALLDAY, 'AllDay'),
+    ]
+
+    date = models.DateField()
+    time = models.CharField(max_length=30, choices=TIMES)
