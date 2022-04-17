e638761ebf6d5a3b487f74462c520e53e981e81e
Mehmet Uzel <98mehmetuzel@gmail.com>
2022-04-18 00:34:32 +0300

    Initial Configurations for new app

--- photographer/photographer/settings.py
+++ photographer/photographer/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'user.apps.UserConfig',
     'homepage.apps.HomepageConfig',
     'appoinment.apps.AppoinmentConfig',
+    'photoshoot.apps.PhotoshootConfig',
     'crispy_forms',
     'import_export',
 ]
--- photographer/photoshoot/views.py
+++ photographer/photoshoot/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def photo_shoot(response):
+    return render(response, "photoshoot/shootplan.html")
