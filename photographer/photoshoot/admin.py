31e94beaf704e103450fad9c31c3d52a0b020a1d
Mehmet Uzel <98mehmetuzel@gmail.com>
2022-04-18 12:52:42 +0300

    New models added and tested
    Also small changes on db design

--- photographer/photoshoot/admin.py
+++ photographer/photoshoot/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Shoot_Type,Concept_Info,Album_Info,Concept,Photo_Concept,Shoot_Plan,Shoot_Appoinment,Shoot_Concept,Payment
 
 # Register your models here.
+
+admin.site.register(Shoot_Type)
+admin.site.register(Concept_Info)
+admin.site.register(Album_Info)
+admin.site.register(Concept)
+admin.site.register(Photo_Concept)
+admin.site.register(Shoot_Plan)
+admin.site.register(Shoot_Appoinment)
+admin.site.register(Shoot_Concept)
+admin.site.register(Payment)
