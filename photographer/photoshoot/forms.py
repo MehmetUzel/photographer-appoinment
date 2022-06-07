f2092b33a0da8b2dd2431489f91d6b5d2bf83f99
Mehmet Uzel <98mehmetuzel@gmail.com>
2022-06-07 22:40:38 +0300

    Update Readme

--- photographer/photoshoot/forms.py
+++ photographer/photoshoot/forms.py
@@ -5,10 +5,14 @@ from .models import Shoot_Plan
 from django.forms import fields
 from django.utils.translation import gettext_lazy as _
 
-class DatePickerInput(forms.DateInput):
-        input_type = 'date'
+class DateInput(forms.DateInput):
+    input_type = 'date'
 
 class ShootPlanForm(forms.ModelForm):
+    birth_date = forms.DateField(
+        input_formats=["%d/%m/%Y"],
+        widget=DateInput(attrs={'placeholder':'dd/mm/yyyy', 'min':'1997/01/01', 'max':'2030/12/31','class': 'datepicker'}, format="%d/%m/%Y"),
+    )
     class Meta:
         model=Shoot_Plan
         fields = ["shoot_type","album_type","num_of_concept","payment_choice","birth_date"]
@@ -18,8 +22,4 @@ class ShootPlanForm(forms.ModelForm):
             'num_of_concept': _('Konsept Sayısı'),
             'birth_date': _('Bebeğin Doğum Tarihi'),
             'payment_choice': _('Ödeme Tercihi'),
-        }
-
-        widgets = {
-            'birth_date' : DatePickerInput(),
         }
